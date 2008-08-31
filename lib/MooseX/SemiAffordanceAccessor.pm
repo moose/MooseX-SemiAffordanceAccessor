31d1505c3bd1fd4544559bec82eaa913e25fa384
Dave Rolsky <autarch@urth.org>
2008-08-31 04:09:07 +0000

    update copyright year

--- lib/MooseX/SemiAffordanceAccessor.pm
+++ lib/MooseX/SemiAffordanceAccessor.pm
@@ -78,7 +78,7 @@ make changes.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Dave Rolsky, All Rights Reserved.
+Copyright 2007-2008 Dave Rolsky, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
